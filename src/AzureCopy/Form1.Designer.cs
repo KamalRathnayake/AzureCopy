76b6e049a6356b8f5a6ca202d830693758fa5ddf
KamalRathnayake <email2kml@gmail.com>
2016-02-13 11:15:31 +0530

    added the source

---
+++ src/AzureCopy/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace AzureCopy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(155, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(269, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "enadocscan";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(155, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(269, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "alocA+CINVXWed1N4MPDmM0b4B03BJV1RhhX/F3bPa0PF2pe2o7AuQP+2ucS10WlvEah5Y8sX5qOU/bmG" +
+    "vxvlQ==";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Storage Account Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Storage Account Key";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Storage Account Key";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Storage Account Name";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(155, 119);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(269, 22);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "vzsK3pouZpAFpRdt9MDlI7qTBymw+vX7TTx5Dft+AAPw3XMM3hksWLj02YMWSJu1IV+lNa5RgLPyneOXV" +
+    "Ln8iw==";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(155, 93);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(269, 22);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.Text = "koolstorage";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "COPY CONTAINERS NAMES";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 216);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(435, 251);
+            this.listBox1.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(179, 178);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 480);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "AzureCopy";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ src/AzureCopy/Form1.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace AzureCopy
+{
+    public enum AccountType
+    {
+        Source, Destination
+    }
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Stopwatch sw;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            printList("Initiating Connections...");
+            var sourceAccountClient = GetAccount(AccountType.Source).CreateCloudBlobClient();
+            var destiAccountClient = GetAccount(AccountType.Destination).CreateCloudBlobClient();
+            sw = new Stopwatch();
+            clearList();
+            sw.Start();
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    label5.Invoke(new Action(() => {
+                        label5.Text = sw.Elapsed.ToString("mm\\:ss");
+                    }));
+                    Thread.Sleep(1000);
+                }
+            });
+            Task.Run(() =>
+            {
+                foreach (CloudBlobContainer cont in sourceAccountClient.ListContainers())
+                {
+                    var targetContainer = destiAccountClient.GetContainerReference(cont.Name);
+                    printList(string.Format("Creating Container {0}...", targetContainer.Name));
+                    targetContainer.CreateIfNotExists();
+
+                    foreach (IListBlobItem srcBlob in cont.ListBlobs(useFlatBlobListing: true))
+                    {
+                        Uri thisBlobUri = srcBlob.Uri;
+                        var serverBlob = sourceAccountClient.GetBlobReferenceFromServer(thisBlobUri);
+
+
+                        CloudBlockBlob sourceBlob = cont.GetBlockBlobReference(serverBlob.Name);
+                        var sharedAccessURI = GetShareAccessUri(serverBlob.Name, 360, cont);
+
+                        CloudBlockBlob targetBlob = targetContainer.GetBlockBlobReference(serverBlob.Name);
+
+                        printList(string.Format(" - Copying the blob {0}...", targetBlob.Name));
+                        targetBlob.StartCopyFromBlob(new Uri(sharedAccessURI));
+                    }
+                }
+            }).ContinueWith((x) => {
+                printList("Done!");
+                sw.Stop();
+            });
+
+            //foreach(var container in sourceAccountClient.ListContainers())
+            //{
+            //    var sourceContainer = sourceAccountClient.GetContainerReference(container.Name);
+            //    var targetContainer = destiAccountClient.GetContainerReference(container.Name);
+            //    targetContainer.CreateIfNotExists();
+
+            //    printList(string.Format("Creating Container {0}...", container.Name));
+
+            //    foreach(var blob in container.ListBlobs())
+            //    {
+            //        ICloudBlob targetBlob=targetContainer.GetBlobReferenceFromServer(blob)
+            //    }
+            //}
+        }
+
+        private string GetShareAccessUri(string blobname,
+                                 int validityPeriodInMinutes,
+                                 CloudBlobContainer container)
+        {
+            var toDateTime = DateTime.Now.AddMinutes(validityPeriodInMinutes);
+
+            var policy = new SharedAccessBlobPolicy
+            {
+                Permissions = SharedAccessBlobPermissions.Read,
+                SharedAccessStartTime = null,
+                SharedAccessExpiryTime = new DateTimeOffset(toDateTime)
+            };
+
+            var blob = container.GetBlockBlobReference(blobname);
+            var sas = blob.GetSharedAccessSignature(policy);
+            return blob.Uri.AbsoluteUri + sas;
+        }
+
+
+        public CloudStorageAccount GetAccount(AccountType type)
+        {
+            StorageCredentials re;
+            if (type == AccountType.Source)
+            {
+                re = new StorageCredentials(textBox1.Text, textBox2.Text);
+                return  new CloudStorageAccount(re, useHttps: true);
+            }
+            else
+            {
+                re = new StorageCredentials(textBox4.Text, textBox3.Text);
+                return new CloudStorageAccount(re, useHttps: true);
+            }
+        }
+        public void msg(object text)
+        {
+            MessageBox.Show(text.ToString());
+        }
+        public void printList(object text)
+        {
+            listBox1.Invoke(new Action(() =>
+            {
+                listBox1.Items.Add(text);
+            }));
+        }
+        public void clearList()
+        {
+            listBox1.Items.Clear();
+        }
+    }
+}
